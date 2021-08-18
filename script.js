2d4711103a80f57e1430e08b58996b3267de1638
Stav Faran <stav_faran@intuit.com>
2021-08-18 15:17:23 +0300

    transformed square into cube

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var squareRotation = 0.0;
+var cubeRotation = 0.0;
 
 
 function drawScene(/** @type {WebGLRenderingContext} */ gl, programInfo, buffers, deltaTime) {
@@ -27,18 +27,20 @@ function drawScene(/** @type {WebGLRenderingContext} */ gl, programInfo, buffers
                     modelViewMatrix,
                     [ 0.0, 0.0, -6.0 ])
 
-    squareRotation += deltaTime;
+                    cubeRotation += deltaTime;
     
 
     mat4.rotate(modelViewMatrix,
                 modelViewMatrix,
-                squareRotation,
+                cubeRotation,
                 [0, 0, 1]);
+
+    mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * .7, [0, 1, 0]);
 
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute.
     {
-        const numComponents = 2;  // pull out 2 values per iteration
+        const numComponents = 3;  // pull out 2 values per iteration
         const type = gl.FLOAT;    // the data in the buffer is 32bit floats
         const normalize = false;  // don't normalize
         const stride = 0;         // how many bytes to get from one set of values to the next
@@ -72,6 +74,10 @@ function drawScene(/** @type {WebGLRenderingContext} */ gl, programInfo, buffers
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
     }
 
+    {
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    }
+
     gl.useProgram(programInfo.program);
 
     // Set the shader uniforms
@@ -87,8 +93,9 @@ function drawScene(/** @type {WebGLRenderingContext} */ gl, programInfo, buffers
 
     {
         const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        const vertexCount = 36;
+        const type = gl.UNSIGNED_SHORT;
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
     }
 
   }
@@ -109,11 +116,42 @@ function clearScene(gl) {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   
     const positions = [
-      -1.0,  1.0,
-       1.0,  1.0,
-      -1.0, -1.0,
-       1.0, -1.0,
-    ];
+        // Front face
+        -1.0, -1.0,  1.0,
+         1.0, -1.0,  1.0,
+         1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+      
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0, -1.0, -1.0,
+      
+        // Top face
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0, -1.0,
+      
+        // Bottom face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+      
+        // Right face
+         1.0, -1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0,  1.0,  1.0,
+         1.0, -1.0,  1.0,
+      
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0,
+      ];
   
     gl.bufferData(gl.ARRAY_BUFFER,
                   new Float32Array(positions),
@@ -124,20 +162,50 @@ function clearScene(gl) {
   
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 
-    const colors = [
-    1.0,  1.0,  1.0,  1.0,    // white
-    1.0,  0.0,  0.0,  1.0,    // red
-    0.0,  1.0,  0.0,  1.0,    // green
-    0.0,  0.0,  1.0,  1.0,    // blue
-    ];
+    const faceColors = [
+        [1.0,  1.0,  1.0,  1.0],    // Front face: white
+        [1.0,  0.0,  0.0,  1.0],    // Back face: red
+        [0.0,  1.0,  0.0,  1.0],    // Top face: green
+        [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+        [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+        [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+      ];
+    
+    // Convert the array of colors into a table for all the vertices.
+
+    var colors = [];
+
+    for (var j = 0; j < faceColors.length; ++j) {
+        const c = faceColors[j];
+
+        // Repeat each color four times for the four vertices of the face
+        colors = colors.concat(c, c, c, c);
+    }
 
     gl.bufferData(gl.ARRAY_BUFFER,
         new Float32Array(colors),
         gl.STATIC_DRAW);
 
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+      ];
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    
+
     return {
       position: positionBuffer,
-      color: colorBuffer
+      color: colorBuffer,
+      indices: indexBuffer
     };
   }
 
